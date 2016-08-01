cc20715eba48c8a1d7a21251eb7c2a7aca60b473
dariol <dariusz.lenartowicz@softio.pl>
2016-08-01 23:41:49 +0200

    Added NullLogger

---
+++ src/Ses.Abstracts/Logging/NullLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace Ses.Abstracts.Logging
+{
+    public class NullLogger : ILogger
+    {
+        public void Trace(string message, params object[] args) { }
+        public void Debug(string message, params object[] args) { }
+        public void Info(string message, params object[] args) { }
+        public void Warn(string message, params object[] args) { }
+        public void Error(string message, params object[] args) { }
+        public void Fatal(string message, params object[] args) { }
+    }
+}
