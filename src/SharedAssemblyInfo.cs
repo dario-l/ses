f5c8bc89e0946305310eec7bf5a2b76762c9b668
Dariusz Lenartowicz <dariusz.lenartowicz@softio.pl>
2016-09-09 23:35:20 +0200

    Version bumped to 0.114.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Dariusz Lenartowicz 2016")]
-[assembly: AssemblyVersion("0.113.0")]
-[assembly: AssemblyFileVersion("0.113.0")]
-[assembly: AssemblyInformationalVersion("0.113.0")]
+[assembly: AssemblyVersion("0.114.0")]
+[assembly: AssemblyFileVersion("0.114.0")]
+[assembly: AssemblyInformationalVersion("0.114.0")]
