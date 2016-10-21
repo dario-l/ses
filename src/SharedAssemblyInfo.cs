3bfc6297c26bb4e29532f8cfbf4bf06b723de2d0
Dariusz Lenartowicz <dariusz.lenartowicz@softio.pl>
2016-10-22 01:17:35 +0200

    0.131.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Dariusz Lenartowicz 2016")]
-[assembly: AssemblyVersion("0.130.0")]
-[assembly: AssemblyFileVersion("0.130.0")]
-[assembly: AssemblyInformationalVersion("0.130.0")]
+[assembly: AssemblyVersion("0.131.0")]
+[assembly: AssemblyFileVersion("0.131.0")]
+[assembly: AssemblyInformationalVersion("0.131.0")]
