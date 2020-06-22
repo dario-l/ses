198faf347ef5cb7d3241992727cf8f5b93980c60
Dariusz Lenartowicz <dariusz.lenartowicz@p2p2.com.pl>
2020-06-22 20:01:13 +0200

    0.164.0 -> 0.165.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Dariusz Lenartowicz 2020")]
-[assembly: AssemblyVersion("0.164.0")]
-[assembly: AssemblyFileVersion("0.164.0")]
-[assembly: AssemblyInformationalVersion("0.164.0")]
+[assembly: AssemblyVersion("0.165.0")]
+[assembly: AssemblyFileVersion("0.165.0")]
+[assembly: AssemblyInformationalVersion("0.165.0")]
